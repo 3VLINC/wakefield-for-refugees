5ccd1bfd9dc22a77c33802c1d2210f94b9174e13
Dan Imbrogno <dan@pmlfreight.tech>
2022-04-07 20:57:27 -0400

    adds blog theme

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -5,7 +5,16 @@ const config: GatsbyConfig = {
     title: `Wakefield For Refugees`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-styled-components"]
+  plugins: [
+    "gatsby-plugin-styled-components",  
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {
+        // basePath defaults to `/`
+        basePath: `/blog`,
+      },
+    }
+  ]
 };
 
 export default config;
