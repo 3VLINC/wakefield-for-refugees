689ff4f10139d5eb9c7d70c37c2c26a96e4e1b4e
Dan Imbrogno <dan@pmlfreight.tech>
2022-09-08 12:05:29 -0400

    Fix link

--- src/shared/GetInvolved.tsx
+++ src/shared/GetInvolved.tsx
@@ -27,11 +27,10 @@ export const GetInvolved: React.FC = () => {
         <div style={{ width: 250 }}><Button href="https://forms.gle/6LD7Bnsq3LMUxwXJ8">Point Person Signup Form</Button></div>
         <Header2>Driver</Header2>
         <p>As newcomers to our rural community, families often need help getting around. Whether it is a trip to the grocery store, a doctor visit, or help apartment hunting, becoming a volunteer driver is a great way to help out without having to make a huge time commitment.</p>
-        <div style={{ width: 250 }}><Button href="https://forms.gle/E5u9ddp5PjyCBkFAA">Driver Signup Form</Button></div>
+        <div style={{ width: 250 }}><Button href="https://forms.gle/WpYZFK1KkX1d7CVb7">Driver Signup Form</Button></div>
         <Header2>Other services</Header2>
         <p>Let us know if you have another skill or ability that would help families settle in to your community. Some examples include jobs, tutoring, translation, social integration, recreation, mental health, child care, accessing other refugee groups, accessing federal and provincial services, social services and more.</p>
         <div style={{ width: 250 }}><Button href="https://forms.gle/ZnbFk3a9dk5Trz5G7">Other Services Signup Form</Button></div>
-
     </MainDiv>
 }
 
